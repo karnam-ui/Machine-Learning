12912334dd8a71c25070c89f91e6135f07b8099b
karnam-ui <24071a66c9@vnrvjiet.in>
2026-02-19 06:22:24 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import pandas as pd
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.model_selection import train_test_split
+
+st.title("Credit Card Fraud Detection(pls have patience, this is a large dataset)")
+
+df = pd.read_csv(r"/Users/suhaas/Downloads/creditcard.csv")
+
+X = df.drop("Class", axis=1)
+y = df["Class"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, stratify=y, random_state=42
+)
+
+model = GradientBoostingClassifier()
+model.fit(X_train, y_train)
+
+threshold = st.slider("Fraud Threshold", 0.0, 1.0, 0.5)
+
+if st.button("Predict on Test Sample"):
+    sample = X_test.iloc[[0]]
+    prob = model.predict_proba(sample)[0][1]
+    prediction = int(prob > threshold)
+
+    st.write("Fraud Probability:", prob)
+    st.write("Prediction:", "Fraud" if prediction else "Legit")
+
