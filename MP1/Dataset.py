077baa2a4e63abc381fc1714d6d97e4cbc65f1d2
karnam-ui <24071a66c9@vnrvjiet.in>
2026-02-24 12:50:53 +0530

    day 1.0

--- MP1/Dataset.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-print("Loading dataset...")
-df = pd.read_csv(r"/Users/suhaas/Downloads/WA_Fn-UseC_-Telco-Customer-Churn.csv")
-df
